3ecf35dda347d5fb821d16fe631d94a43fc3c38e
suehyunpark <tngus1499@gmail.com>
2022-08-22 04:37:07 +0000

    Remove unnecessary comments

--- src/json2csv.py
+++ src/json2csv.py
@@ -23,17 +23,3 @@ if __name__ == "__main__":
     json2csv(read_path, write_path, orient, field)
     
     # orient=index for *word_freq.json
-
-'''
-with open("output/wmt/rare_words.json", 'r') as j:
-    rare_words = json.load(j)
-    
-with open("output/wmt/unseen_words.json", 'r') as j:
-    unseen_words = json.load(j)
-    
-rare_words_df = pd.json_normalize(rare_words["rare_words"])
-unseen_words_df = pd.json_normalize(unseen_words["unseen_words"])
-
-rare_words_df.to_csv("output/wmt/rare_words.csv")
-unseen_words_df.to_csv("output/wmt/unseen_words.csv")
-'''
